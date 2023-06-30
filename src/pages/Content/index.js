abc99000959ce2d15b237166d6012f6827fcd550
xlearns <2447684095@qq.com>
2023-06-30 15:30:51 +0800

    fix: the drag and drop function failed for the first time

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,24 +1,20 @@
 import { printLine } from './modules/print';
-import { funDrag,storage } from '@/share'
+import { funDrag, storage } from '@/share';
 
-const name = 'ngpiiejhejikkflajojekhfnfpanfbfg'
+const name = 'ngpiiejhejikkflajojekhfnfpanfbfg';
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
 
-
-
-
 (async () => {
-    const result = await chrome.runtime.sendMessage({ action: 'get_mouse_data' })
-    const {x,y} = result?.mouse_position || {}
-    const position = {x:x||'50%',y:y||'50%'}
-    console.log(position)
-    const newDiv = document.createElement('div');
-    newDiv.setAttribute('title', '长按可拖动到任何位置');
-    newDiv.setAttribute('id', 'chrome-extension-boilerplate-react');
-    newDiv.style.cssText = `
+  const result = await chrome.runtime.sendMessage({ action: 'get_mouse_data' });
+  const { x, y } = result?.mouse_position || {};
+  const position = { x: x || '50%', y: y || '50%' };
+  const newDiv = document.createElement('div');
+  newDiv.setAttribute('title', '长按可拖动到任何位置');
+  newDiv.setAttribute('id', 'chrome-extension-boilerplate-react');
+  newDiv.style.cssText = `
         position: fixed; 
         z-index: 2147483647;
         width: 64px;
@@ -29,7 +25,7 @@ printLine("Using the 'printLine' function from the Print Module");
         cursor: pointer; 
         border-radius: 8px; 
         background: url(chrome-extension://${name}/logo.png) no-repeat 0 0/100%;
-    `
-    funDrag(newDiv)
-    document.body.appendChild(newDiv);
-})()
+    `;
+  document.body.appendChild(newDiv);
+  funDrag(newDiv);
+})();
--- src/share/index.js
+++ src/share/index.js
@@ -1,146 +1,150 @@
 /**
- * 
- * @param {*} fn 
- * @returns 
+ *
+ * @param {*} fn
+ * @returns
  */
-export const isFunction = (fn)=>{
-    return typeof fn === 'function'
-}
+export const isFunction = (fn) => {
+  return typeof fn === 'function';
+};
 /**
- * 
- * @param {*} msg 
+ *
+ * @param {*} msg
  */
-export const rnSendMessage =  (msg)=>{
-    chrome.runtime.sendMessage(msg)
-}
-
-/**
- * 
- * @param {*} fn 
- */
-export const rnMessageListener = (fn)=>{
-    chrome.runtime.onMessage.addListener((...arg)=>{
-        isFunction(fn) && fn(...arg)
-    }); 
-}
-/**
- * 
- * @param {*} id 
- * @param {*} msg 
- */
-export const tabSendMessage = (id,msg)=>{
-    chrome.tabs.sendMessage(id, msg);
-}
-
-/**
- * 
- * @param {*} config 
- */
-export const tabsExecJs = (config)=>{
-    const { tabId,...args } = Object.assign({},config);
-    chrome.scripting.executeScript({
-        target: {tabId},
-        ...args
-    })
-}
-
-/**
- * 
- * @param {*} fn 
- * @param {*} config 
- */
-export const getTabs = (config)=>{
-    const arg = Object.assign({},{ active: true, currentWindow: true },config)
-    return new Promise((resolve)=>{
-        chrome.tabs.query(arg, function(tabs) {
-            resolve(tabs)
-        })
-    })
-    
-}
-
-
-export const storage = {
-    set(key,value,fn){
-        chrome.storage.local.set({ [key]: value },()=>{
-            isFunction(fn) && fn()
-        });
-    },
-    get(key){
-        return new Promise((resolve)=>{
-            chrome.storage.local.get([key], (result) => {
-                const value = result.key;
-                resolve(value)
-              });
-        })
-    },
-    getAll(){
-        return new Promise((resolve)=>{
-            chrome.storage.local.get(null, function(items) {
-                resolve(items);
-            });
-        })
-    },
-    clear(){
-        return chrome.storage.local.clear()
-    }
-}
-
-
-export function funDrag(element, callback) {
-    callback = callback || function() {};
-    var params = {
-        left: 0,
-        top: 0,
-        currentX: 0,
-        currentY: 0,
-        flag: false
-    };
-    //获取相关CSS属性
-    var getCss = function(o,key){
-        return o.currentStyle? o.currentStyle[key] : document.defaultView.getComputedStyle(o,false)[key];     
-    };
-    
-    //拖拽的实现
-    if(getCss(element, "left") !== "auto"){
-        params.left = getCss(element, "left");
-    }
-    if(getCss(element, "top") !== "auto"){
-        params.top = getCss(element, "top");
-    }
-    //o是移动对象
-    element.onmousedown = function(event){
-        params.flag = true;
-        event = event || window.event;
-        params.currentX = event.clientX;
-        params.currentY = event.clientY;
-    };
-    document.onmouseup = function(){
-        params.flag = false;    
-        if(getCss(element, "left") !== "auto"){
-            params.left = getCss(element, "left");
-        }
-        if(getCss(element, "top") !== "auto"){
-            params.top = getCss(element, "top");
-        }
-        callback();
-    };
-    document.onmousemove = function(event){
-        event = event || window.event;
-        if(params.flag){
-            var nowX = event.clientX, nowY = event.clientY;
-            var disX = nowX - params.currentX, disY = nowY - params.currentY;
-            element.style.left = parseInt(params.left) + disX + "px";
-            element.style.top = parseInt(params.top) + disY + "px";
-            rnSendMessage({ action: "mouse_position", position: { x: element.style.left, y:  element.style.top } });
-        }
-    }    
+export const rnSendMessage = (msg) => {
+  chrome.runtime.sendMessage(msg);
 };
 
-export function  sendMessageToContentScript(message, callback) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
-        if (callback) callback(response)
-      })
-    })
+/**
+ *
+ * @param {*} fn
+ */
+export const rnMessageListener = (fn) => {
+  chrome.runtime.onMessage.addListener((...arg) => {
+    isFunction(fn) && fn(...arg);
+  });
+};
+/**
+ *
+ * @param {*} id
+ * @param {*} msg
+ */
+export const tabSendMessage = (id, msg) => {
+  chrome.tabs.sendMessage(id, msg);
+};
+
+/**
+ *
+ * @param {*} config
+ */
+export const tabsExecJs = (config) => {
+  const { tabId, ...args } = Object.assign({}, config);
+  chrome.scripting.executeScript({
+    target: { tabId },
+    ...args,
+  });
+};
+
+/**
+ *
+ * @param {*} fn
+ * @param {*} config
+ */
+export const getTabs = (config) => {
+  const arg = Object.assign({}, { active: true, currentWindow: true }, config);
+  return new Promise((resolve) => {
+    chrome.tabs.query(arg, function (tabs) {
+      resolve(tabs);
+    });
+  });
+};
+
+export const storage = {
+  set(key, value, fn) {
+    chrome.storage.local.set({ [key]: value }, () => {
+      isFunction(fn) && fn();
+    });
+  },
+  get(key) {
+    return new Promise((resolve) => {
+      chrome.storage.local.get([key], (result) => {
+        const value = result.key;
+        resolve(value);
+      });
+    });
+  },
+  getAll() {
+    return new Promise((resolve) => {
+      chrome.storage.local.get(null, function (items) {
+        resolve(items);
+      });
+    });
+  },
+  clear() {
+    return chrome.storage.local.clear();
+  },
+};
+
+export function funDrag(element, callback) {
+  callback = callback || function () {};
+  var params = {
+    left: 0,
+    top: 0,
+    currentX: 0,
+    currentY: 0,
+    flag: false,
+  };
+  //获取相关CSS属性
+  var getCss = function (o, key) {
+    return o.currentStyle
+      ? o.currentStyle[key]
+      : document.defaultView.getComputedStyle(o, false)[key];
+  };
+
+  //拖拽的实现
+  if (getCss(element, 'left') !== 'auto') {
+    params.left = parseInt(getCss(element, 'left'));
   }
+  if (getCss(element, 'top') !== 'auto') {
+    params.top = parseInt(getCss(element, 'top'));
+  }
+  //o是移动对象
+  element.addEventListener('mousedown', function (event) {
+    params.flag = true;
+    event = event || window.event;
+    params.currentX = event.clientX;
+    params.currentY = event.clientY;
+  });
+  element.addEventListener('mouseup', function () {
+    params.flag = false;
+    if (getCss(element, 'left') !== 'auto') {
+      params.left = parseInt(getCss(element, 'left'));
+    }
+    if (getCss(element, 'top') !== 'auto') {
+      params.top = parseInt(getCss(element, 'top'));
+    }
+    callback();
+  });
+  document.addEventListener('mousemove', function (event) {
+    event = event || window.event;
+    if (params.flag) {
+      var nowX = event.clientX,
+        nowY = event.clientY;
+      var disX = nowX - params.currentX,
+        disY = nowY - params.currentY;
+      element.style.left = params.left + disX + 'px';
+      element.style.top = params.top + disY + 'px';
+      rnSendMessage({
+        action: 'mouse_position',
+        position: { x: element.style.left, y: element.style.top },
+      });
+    }
+  });
+}
+
+export function sendMessageToContentScript(message, callback) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
+      if (callback) callback(response);
+    });
+  });
+}
